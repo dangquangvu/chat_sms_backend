936103a343d23d874826c426b7aba7f9f1215bb1
master vux <dangquangvu999@gmail.com>
2020-01-17 11:22:09 +0700

    fdsfadsf

---
+++ schema/friend.schema.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const moment = require("moment");
+const bcrypt = require("bcryptjs");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        match: [/.+@.+\..+/, "Địa chỉ email không hợp lệ"]
+    },
+    password: {
+        type: String
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"]
+    },
+    created: {
+        type: Date,
+        default: Date.now()
+    },
+    updated: {
+        type: Date,
+        default: Date.now()
+    }
+}, { usePushEach: true });
+UserSchema.pre("save", function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified("password")) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword) {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, this.password).then(data => {
+            return resolve(data);
+        });
+    });
+};
+
+module.exports = mongoose.model("User", UserSchema);
--- schema/index.js
+++ schema/index.js
@@ -1,6 +1,3 @@
 module.exports = {
-    User: require("./user.schema"),
-    FavoriteMusic: require("./list_music_favorite.schema"),
-    Songs: require("./music.schema"),
-    Category: require("./Category.schema")
+    User: require("./user.schema")
 };
